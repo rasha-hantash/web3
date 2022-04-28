4e6b8b962df4d66cfd13342f5c04b7366fe19785
Rasha Saadeh <>
2022-04-28 00:12:32 -0400

    initial commit

---
+++ dapp/nft-marketplace/tailwind.config.js
@@ -0,0 +1,12 @@
+/* tailwind.config.js */
+module.exports = {
+  content: [
+    // where to apply tailwind styling
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
